4ed9c8039204ed995cc969130f03c81c3ad95b95
Christopher Sexton <cpsexton@gmail.com>
2019-08-09 18:15:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,25 @@
 # Your Code Here
-def map(source_array, n)
-  yeild(source_array)
+def map(source)
+  new = []
+  i = 0
+  while i < source.length
+    new.push(yield(source[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(source, x=nil)
+  if x
+    accum = x
+    i = 0
+  else
+    accum = source[0]
+    i = 1
+  end
+  while i < source.length
+    accum = yield(accum, source[i])
+    i += 1
+  end
+  accum
 end
